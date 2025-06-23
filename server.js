a7edad67a577349951758592bc396e3ef8273f41
Cheruku Manikanta Reddy <manikantareddy@Cherukus-MacBook-Air.local>
2025-06-23 15:38:30 +0530

    Cors Integration

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express'); // // Include the express module
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./src/routes/authRoutes');
+const cors = require('cors');
 
 const app = express(); // Instantiate express app.
 
 app.use(express.json()); //Middleware to conver json to javascript object.
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    Credentials: true
+};
+app.use(cors(corsOptions));
 app.use('/auth', authRoutes);
 
 const PORT = 5001;
